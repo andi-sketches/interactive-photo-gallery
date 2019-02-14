b16270f41120e76882fbbbafa887938cbdb043d3
Andi <andivagyoken@gmail.com>
2019-02-14 16:36:13 +0100

    Changed event listener to have "submit" functionality on entire form

--- js/script.js
+++ js/script.js
@@ -11,18 +11,35 @@ lightbox.option({
 
 //Search functionality
 const images = document.querySelectorAll('.image-link');
+const form = document.getElementById('sticker')
+const input = document.querySelector('input')
 let description = "";
 
-$(document).ready(function() {
-  $('.search-box').on('keyup', function() {
-    let $search = $(this).val().toLowerCase();
-    for (let i = 0; i < images.length; i++) {
-      description = images[i].getAttribute('data-title');
-      if (description.toLowerCase().indexOf($search) > -1) {
-        images[i].style.display = '';
-      } else {
-        images[i].style.display = 'none';
-      }
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let search = input.value.toLowerCase();
+  for (let i = 0; i < images.length; i++) {
+    description = images[i].getAttribute('data-title');
+    if (description.toLowerCase().indexOf(search) > -1) {
+      images[i].style.display = '';
+    } else {
+      images[i].style.display = 'none';
     }
-  })
+  }
 });
+
+  /*
+  $(document).ready(function() {
+    $('.search-input').on('keyup', function() {
+      let $search = $(this).val().toLowerCase();
+      for (let i = 0; i < images.length; i++) {
+        description = images[i].getAttribute('data-title');
+        if (description.toLowerCase().indexOf($search) > -1) {
+          images[i].style.display = '';
+        } else {
+          images[i].style.display = 'none';
+        }
+      }
+    })
+  });
+  */
